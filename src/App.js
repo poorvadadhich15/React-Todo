99374efd9f70c941d33e1f4ed0e510a3dbb10084
Poorva <poorvadadhich15@gmail.com>
2022-07-06 15:05:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import react, { useState, useEffect } from "react";
+import Header from "./components/Header";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+import "./App.css";
+
+const App = () => {
+  const initialState = JSON.parse(localStorage.getItem("todo")) || [];
+  const [input, setInput] = useState("");
+  const [todo, setTodo] = useState(initialState);
+  const [editTodo, setEditTodo] = useState(null);
+
+  useEffect(() => {
+    localStorage.setItem("todo", JSON.stringify(todo));
+  }, [todo]);
+  return (
+    <div className="container">
+      <div className="app-wrapper">
+        <div>
+          <Header />
+        </div>
+        <div>
+          <Form
+            input={input}
+            setInput={setInput}
+            todo={todo}
+            setTodo={setTodo}
+            editTodo={editTodo}
+            setEditTodo={setEditTodo}
+          />
+        </div>
+        <div>
+          <TodoList todo={todo} setTodo={setTodo} setEditTodo={setEditTodo} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
